a1a1bd80116da5486ebffd2e49f7a2dfa481d82d
Muhammad Asim <47195175+muhamdasim@users.noreply.github.com>
2020-09-20 04:29:59 +0500

    Updates

--- seniorly_main.py
+++ seniorly_main.py
@@ -1,6 +1,12 @@
 import seniorly_scraper as scraper
-
+import pandas as pd
+import csv
 urls=[]
+df=pd.read_csv('urls.csv')
+
+for i in df['urls']:
+    urls.append(i)
+
 
 pageTitle=[]
 metaDescription=[]
@@ -15,8 +21,24 @@ AmenitiesProvided=[]
 pricingStartsFrom=[]
 pricingByRoomType=[]
 
+
+def save():
+    with open("sample.csv", "w", newline='', encoding="utf-8") as csvFile:
+        fieldnames = ['pageTitle', 'metaDescription', 'communityName','communityStreetAddress','communityZipCode','communityState',
+        'images','content','careTypes','Amenities','pricingStartsFrom','pricingByRoomType']
+        writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
+        writer.writeheader()
+        for h, d, c in zip(pageTitle,metaDescription,communityName,communityStreetAddres,communityZipCode,communityState
+                           ,images,content,careTypesProvided,AmenitiesProvided,pricingStartsFrom,pricingByRoomType):
+            writer.writerow({'pageTitle':pageTitle, 'metaDescription':metaDescription, 'communityName':communityName,'communityStreetAddress':communityStreetAddres,'communityZipCode':communityZipCode,'communityState':communityState,
+        'images':images,'content':content,'careTypes':careTypesProvided,'Amenities':AmenitiesProvided,'pricingStartsFrom':pricingStartsFrom,'pricingByRoomType':pricingByRoomType})
+
+
+counter=0
+
 for i in urls:
-    try:
+
+        print("Scraping No:",counter,i)
         soup=scraper.pageRequests(i)
         ad=scraper.getCommunityStreetAddress(soup)
         communityStreetAddres.append(ad)
@@ -26,10 +48,15 @@ for i in urls:
         communityZipCode.append(scraper.getCommunityZipCode(soup))
         communityState.append(scraper.getCommunityState(soup))
         images.append(scraper.getImages(soup))
-        content.append(scraper.getCommunityContent(soup))
+        #content.append(scraper.getCommunityContent(soup))
         careTypesProvided.append(scraper.getCareTypesProvided(soup))
         AmenitiesProvided.append(scraper.getAmenitiesProvided(soup))
         pricingStartsFrom.append(scraper.getPricingStartsFrom(soup))
         pricingByRoomType.append(scraper.getPricingByRoomType(soup))
-    except:
-        continue
+        counter+=1
+        save()
+
+
+
+
+
--- seniorly_scraper.py
+++ seniorly_scraper.py
@@ -1,105 +1,111 @@
-# import requests
-# import bs4
-# import re
-# import json
-# import pandas as pd
-#
-#
-#
-# def listToString(s):
-#     # initialize an empty string
-#     str1 =''
-#
-#     # return string
-#     for i in s:
-#         str1+=i+', '
-#
-#     return str1[:-2]
-#
-# def pageRequests(url):
-#     r=requests.get(url)
-#     soup = bs4.BeautifulSoup(r.text, 'lxml')
-#     return soup
-#
-# def getPageTitle(soup):
-#     return soup.title.string.strip()
-#
-# def getMetaDescription(soup):
-#     meta = soup.find_all('meta')
-#     return listToString([meta.attrs['content'] for meta in meta if 'name' in meta.attrs and meta.attrs['name'] == 'description'])
-#
-# def getCommunityName(soup):
-#     return soup.find(class_='Root__H1-sc-1lnoi1-0 iCGCDp Root-sc-1lnoi1-4 eaYBkV Heading-p8la3x-0 irVoEB').get_text().strip()
-#
-# def getCommunityStreetAddress(soup):
-#     return soup.find(class_='Root__H2-sc-1lnoi1-1 eaHtqb Root-sc-1lnoi1-4 jBwoEe Heading-p8la3x-0 irVoEB').get_text().strip()
-#
-# def getCommunityZipCode(soup):
-#     data=soup.find(class_='Root__H2-sc-1lnoi1-1 eaHtqb Root-sc-1lnoi1-4 jBwoEe Heading-p8la3x-0 irVoEB').get_text().strip()
-#     return re.sub("[^\w]", " ", data).split()[-1]
-#
-#
-#     return city
-# def getCommunityState(soup):
-#     data = soup.find(
-#         class_='Root__H2-sc-1lnoi1-1 eaHtqb Root-sc-1lnoi1-4 jBwoEe Heading-p8la3x-0 irVoEB').get_text().strip()
-#     state= ''
-#
-#     return re.sub("[^\w]", " ", data).split()[-2]
-#
-# def getCommunityCity(soup):
-#     data = soup.find(
-#         class_='Root__H2-sc-1lnoi1-1 eaHtqb Root-sc-1lnoi1-4 jBwoEe Heading-p8la3x-0 irVoEB').get_text().strip()
-#     state = ''
-#
-#     return re.sub("[^\w]", " ", data).split()[-3]
-#
-# def getImages(soup):
-#     dt=[]
-#     for i in soup.findAll(class_='ResponsiveImage__ResponsiveWrapper-l4g8bp-0 jscNWe'):
-#         dt.append(i.find('img').get('data-src'))
-#
-#     return listToString(dt)
-#
-# def getCommunityContent(soup):
-#     return soup.find(class_='CollapsibleBlock__BlockCap-s326rf-1 jhhVKz').find(class_='Paragraph__StyledP-sc-17r02x6-0 ONtKM').get_text().strip()
-#
-# def getCareTypesProvided(soup):
-#     dt=[]
-#     for i in soup.find(class_='CommunityCareService__Wrapper-sc-1uu6i2h-0 bDIQtl').findAll(class_='Root-sc-1m9vk1w-0 gCatle Block-sc-1184las-0 IconItem__Wrapper-sc-155qj67-0 ivNDcb'):
-#         dt.append(i.find(class_='Root-sc-1m9vk1w-0 gCatle Block-sc-1184las-0 jqgkYL').get_text().strip())
-#
-#
-#
-#     return listToString(dt)
-#
-#
-# def getAmenitiesProvided(soup):
-#     dt=[]
-#     for i in soup.find(class_='CommunityAmenities__Wrapper-sc-1gihmpf-0 RJvAA').findAll(class_='Root-sc-1m9vk1w-0 gCatle Block-sc-1184las-0 IconItem__Wrapper-sc-155qj67-0 ivNDcb'):
-#         dt.append(i.find(class_='Root-sc-1m9vk1w-0 gCatle Block-sc-1184las-0 jqgkYL').get_text().strip())
-#
-#
-#
-#     return listToString(dt)
-#
-#
-# def getPricingStartsFrom(soup):
-#     return soup.find(class_='Span-vzvmw4-0 jYbTJi').get_text().strip()
-#
-# def getPricingByRoomType(soup):
-#     dt=[]
-#     for i in soup.find(class_='CommunityPricingTable__StyledTable-r1omm4-4 lbwBwC').findAll('tr'):
-#         for k in i.findAll('td'):
-#             dt.append(k.get_text())
-#
-#
-#     dt.remove('Type')
-#     dt.remove('Average Monthly Cost*')
-#
-#     return listToString(dt)
+import requests
+import bs4
+import re
+import json
+import pandas as pd
 
-animal = "Hippopotamus"
-print(animal[3:6])
-print(animal[-5])
-print(animal[10:])
+
+
+def listToString(s):
+    # initialize an empty string
+    str1 =''
+
+    # return string
+    for i in s:
+        str1+=i+', '
+
+    return str1[:-2]
+
+def pageRequests(url):
+    r=requests.get(url)
+    soup = bs4.BeautifulSoup(r.text, 'lxml')
+    return soup
+
+def getPageTitle(soup):
+    return soup.title.string.strip()
+
+def getMetaDescription(soup):
+    meta = soup.find_all('meta')
+    return listToString([meta.attrs['content'] for meta in meta if 'name' in meta.attrs and meta.attrs['name'] == 'description'])
+
+def getCommunityName(soup):
+    return soup.find(class_='Root__H1-sc-1lnoi1-0 iCGCDp Root-sc-1lnoi1-4 eaYBkV Heading-p8la3x-0 irVoEB').get_text().strip()
+
+def getCommunityStreetAddress(soup):
+    return soup.find(class_='Root__H2-sc-1lnoi1-1 eaHtqb Root-sc-1lnoi1-4 jBwoEe Heading-p8la3x-0 irVoEB').get_text().strip()
+
+def getCommunityZipCode(soup):
+    data=soup.find(class_='Root__H2-sc-1lnoi1-1 eaHtqb Root-sc-1lnoi1-4 jBwoEe Heading-p8la3x-0 irVoEB').get_text().strip()
+    return re.sub("[^\w]", " ", data).split()[-1]
+
+
+    return city
+def getCommunityState(soup):
+    data = soup.find(
+        class_='Root__H2-sc-1lnoi1-1 eaHtqb Root-sc-1lnoi1-4 jBwoEe Heading-p8la3x-0 irVoEB').get_text().strip()
+    state= ''
+
+    return re.sub("[^\w]", " ", data).split()[-2]
+
+def getCommunityCity(soup):
+    data = soup.find(
+        class_='Root__H2-sc-1lnoi1-1 eaHtqb Root-sc-1lnoi1-4 jBwoEe Heading-p8la3x-0 irVoEB').get_text().strip()
+    state = ''
+
+    return re.sub("[^\w]", " ", data).split()[-3]
+
+def getImages(soup):
+    dt=[]
+    for i in soup.findAll(class_='ResponsiveImage__ResponsiveWrapper-l4g8bp-0 jscNWe'):
+        dt.append(i.find('img').get('data-src'))
+
+    return listToString(dt)
+
+def getCommunityContent(soup):
+    try:
+        return soup.find(class_='CollapsibleBlock__BlockCap-s326rf-1 jhhVKz').find(class_='Paragraph__StyledP-sc-17r02x6-0 ONtKM').get_text().strip()
+    except:
+        return -1
+def getCareTypesProvided(soup):
+    try:
+        dt=[]
+        for i in soup.find(class_='CommunityCareService__Wrapper-sc-1uu6i2h-0 bDIQtl').findAll(class_='Root-sc-1m9vk1w-0 gCatle Block-sc-1184las-0 IconItem__Wrapper-sc-155qj67-0 ivNDcb'):
+            dt.append(i.find(class_='Root-sc-1m9vk1w-0 gCatle Block-sc-1184las-0 jqgkYL').get_text().strip())
+
+
+
+        return listToString(dt)
+
+    except:
+        return  -1
+
+
+def getAmenitiesProvided(soup):
+    try:
+        dt=[]
+        for i in soup.find(class_='CommunityAmenities__Wrapper-sc-1gihmpf-0 RJvAA').findAll(class_='Root-sc-1m9vk1w-0 gCatle Block-sc-1184las-0 IconItem__Wrapper-sc-155qj67-0 ivNDcb'):
+            dt.append(i.find(class_='Root-sc-1m9vk1w-0 gCatle Block-sc-1184las-0 jqgkYL').get_text().strip())
+
+
+
+        return listToString(dt)
+
+    except:
+        return -1
+
+
+def getPricingStartsFrom(soup):
+    return soup.find(class_='Span-vzvmw4-0 jYbTJi').get_text().strip()
+
+def getPricingByRoomType(soup):
+    dt=[]
+    for i in soup.find(class_='CommunityPricingTable__StyledTable-r1omm4-4 lbwBwC').findAll('tr'):
+        for k in i.findAll('td'):
+            dt.append(k.get_text())
+
+
+    dt.remove('Type')
+    dt.remove('Average Monthly Cost*')
+
+    return listToString(dt)
+
