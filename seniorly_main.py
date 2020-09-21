f6a009db20677c0338c5b9d721ce5365ad0106f9
Muhammad Asim <47195175+muhamdasim@users.noreply.github.com>
2020-09-21 18:02:05 +0500

    Updates

--- seniorly_main.py
+++ seniorly_main.py
@@ -23,38 +23,45 @@ pricingByRoomType=[]
 
 url=[]
 
+def saveData(u,pageTitle,metaDescription,communityName,communityStreetAddres,communityZipCode,communityState,images,content,careTypesProvided,AmenitiesProvided,pricingStartsFrom,pricingByRoomType):
+    with open("sample.csv", "w", newline='', encoding="utf-8") as csvFile:
+        fieldnames = ['url', 'pageTitle', 'metaDescription', 'communityName', 'communityStreetAddress',
+                      'communityZipCode', 'communityState',
+                      'images', 'content', 'careTypes', 'Amenities', 'pricingStartsFrom', 'pricingByRoomType']
+        writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
+        writer.writeheader()
+        for u, pageTitle, metaDescription, communityName, communityStreetAddres, communityZipCode, communityState, images, content, careTypesProvided, AmenitiesProvided, pricingStartsFrom, pricingByRoomType in zip(
+                url, pageTitle, metaDescription, communityName, communityStreetAddres, communityZipCode, communityState
+                , images, content, careTypesProvided, AmenitiesProvided, pricingStartsFrom, pricingByRoomType):
+            writer.writerow(
+                {'url': u, 'pageTitle': pageTitle, 'metaDescription': metaDescription, 'communityName': communityName,
+                 'communityStreetAddress': communityStreetAddres, 'communityZipCode': communityZipCode,
+                 'communityState': communityState,
+                 'images': images, 'content': content, 'careTypes': careTypesProvided, 'Amenities': AmenitiesProvided,
+                 'pricingStartsFrom': pricingStartsFrom, 'pricingByRoomType': pricingByRoomType})
 
 
 for i in urls:
+        try:
+            print("Scraping No:",i)
+            soup=scraper.pageRequests(i)
+            ad=scraper.getCommunityStreetAddress(soup)
+            communityStreetAddres.append(ad)
+            url.append(i)
+            pageTitle.append(scraper.getPageTitle(soup))
+            metaDescription.append(scraper.getMetaDescription(soup))
+            communityName.append(scraper.getCommunityName(soup))
+            communityZipCode.append(scraper.getCommunityZipCode(soup))
+            communityState.append(scraper.getCommunityState(soup))
+            images.append(scraper.getImages(soup))
+            content.append(scraper.getCommunityContent(soup))
+            careTypesProvided.append(scraper.getCareTypesProvided(soup))
+            AmenitiesProvided.append(scraper.getAmenitiesProvided(soup))
+            pricingStartsFrom.append(scraper.getPricingStartsFrom(soup))
+            pricingByRoomType.append(scraper.getPricingByRoomType(soup))
+            saveData(url,pageTitle,metaDescription,communityName,communityStreetAddres,communityZipCode,communityState,images,content,careTypesProvided,AmenitiesProvided,pricingStartsFrom,pricingByRoomType)
 
-        print("Scraping No:",i)
-        soup=scraper.pageRequests(i)
-        ad=scraper.getCommunityStreetAddress(soup)
-        communityStreetAddres.append(ad)
-        url.append(i)
-        pageTitle.append(scraper.getPageTitle(soup))
-        metaDescription.append(scraper.getMetaDescription(soup))
-        communityName.append(scraper.getCommunityName(soup))
-        communityZipCode.append(scraper.getCommunityZipCode(soup))
-        communityState.append(scraper.getCommunityState(soup))
-        images.append(scraper.getImages(soup))
-        content.append(scraper.getCommunityContent(soup))
-        careTypesProvided.append(scraper.getCareTypesProvided(soup))
-        AmenitiesProvided.append(scraper.getAmenitiesProvided(soup))
-        pricingStartsFrom.append(scraper.getPricingStartsFrom(soup))
-        pricingByRoomType.append(scraper.getPricingByRoomType(soup))
-
-
-
-
-with open("sample.csv", "w", newline='', encoding="utf-8") as csvFile:
-        fieldnames = ['url','pageTitle', 'metaDescription', 'communityName','communityStreetAddress','communityZipCode','communityState',
-        'images','content','careTypes','Amenities','pricingStartsFrom','pricingByRoomType']
-        writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
-        writer.writeheader()
-        for u,pageTitle,metaDescription,communityName,communityStreetAddres,communityZipCode,communityState,images,content,careTypesProvided,AmenitiesProvided,pricingStartsFrom,pricingByRoomType  in zip(url,pageTitle,metaDescription,communityName,communityStreetAddres,communityZipCode,communityState
-                           ,images,content,careTypesProvided,AmenitiesProvided,pricingStartsFrom,pricingByRoomType):
-            writer.writerow({'url':u,'pageTitle':pageTitle, 'metaDescription':metaDescription, 'communityName':communityName,'communityStreetAddress':communityStreetAddres,'communityZipCode':communityZipCode,'communityState':communityState,
-        'images':images,'content':content,'careTypes':careTypesProvided,'Amenities':AmenitiesProvided,'pricingStartsFrom':pricingStartsFrom,'pricingByRoomType':pricingByRoomType})
+        except:
+            continue
 
 
